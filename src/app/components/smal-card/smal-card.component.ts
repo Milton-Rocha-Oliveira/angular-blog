71fb34b8900319f52118c5df84697b1bdff0e738
milton rocha <milton.j.r.oliveira.1990@gmail.com>
2024-11-02 11:00:05 -0300

    components create

---
+++ src/app/components/smal-card/smal-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smal-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './smal-card.component.html',
+  styleUrl: './smal-card.component.css'
+})
+export class SmalCardComponent {
+
+}
